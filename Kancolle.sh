c26858bb3997d737b8ba54f51daa247e05891fdb
Miguel Silvino <miguelslvn@gmail.com>
2023-04-23 02:18:22 -0700

    Update Kancolle.sh

--- Kancolle.sh
+++ Kancolle.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/deck/KCCP/KanColle-English-Patch-KCCP/
+git pull
 cd /home/deck/KCCP/KCCacheProxy-linux/
 konsole --fullscreen -e /home/deck/KCCP/KCCacheProxy-linux/proxy-linux &
 gtk-launch com.google.Chrome.desktop
